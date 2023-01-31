bdf2a9b59cb339615084c38e8804c75cfb910210
Hafid Iqbal Malik CSE-20-Le-67 <97093879+hafidmalik@users.noreply.github.com>
2023-02-01 00:33:32 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function Nextra({ Component, pageProps }) {
         <link
           rel="alternate"
           type="application/rss+xml"
-          title="RSS"
+          title="Hafid"
           href="/feed.xml"
         />
         <link
